f58a61a511fdbafa853d49832eb992793a5b4615
Becha <zied.becha@eurecom.fr>
2020-05-26 00:22:22 +0200

    few additions

--- test.py
+++ test.py
@@ -5,6 +5,23 @@ import pefile
 def usage():
 	print ('test.py -i <inputfile> [-a] [-s Header]')
 
+def help():
+	print("-i : Input File")
+	print("-a : Display all headers")
+	print("-s header : Display a specific header")
+	print("------------------------------------")
+	print("[File Headers]")
+	print(" Machine\n","NumberOfSections\n", "PointerToSymbolTable\n"
+		,"NumberOfSymbols\n","SizeOfOptionalHeader\n","Characteristics\n")
+	print("------------------------------------")
+	print("[Optional Headers]")
+	print(" Magic\n","MajorLinkerVersion\n","MinorLinkerVersion\n","SizeOfCode\n","SizeOfInitializedData\n"    
+		,"SizeOfUninitializedData\n","AddressOfEntryPoint\n","BaseOfCode\n","BaseOfData\n","ImageBase\n"                 
+		,"SectionAlignment\n","FileAlignment\n","MajorOperatingSystemVersion\n","MinorOperatingSystemVersion\n"
+		,"MajorImageVersion\n","MinorImageVersion\n","MajorSubsystemVersion\n","MinorSubsystemVersion\n"     
+		,"Reserved1\n","SizeOfImage\n","SizeOfHeaders\n","CheckSum\n","Subsystem\n","DllCharacteristics\n"        
+		,"SizeOfStackReserve\n","SizeOfStackCommit\n","SizeOfHeapReserve\n","SizeOfHeapCommit\n"          
+		,"LoaderFlags\n","NumberOfRvaAndSizes\n")
 
 def display(pe,arg):
 	if arg=="TimeDateStamp":
@@ -22,13 +39,35 @@ def display(pe,arg):
 		print(arg+" : 0x%0X" % pe.OPTIONAL_HEADER.dump_dict()[arg]['Value'])
 	else:
 		print("Please enter a valid argument")
-		print("refter to ...") #TODO
+		print("-----------------------------")
+		help()
+
+def fstrings(pe,min=4):
+	print(pe)
+
+	########TODO########
+#	with open(path, errors="ignore") as f:
+#		for c in f.read():
+#			print(c)
+#			if c in string.printable:
+#				result += c
+#				continue
+#			if len(result) >= min:
+#				yield result
+#			result = ""
+#			if len(result) >= min:  # catch result at EOF
+#				yield result
+#	return result
+
+def flibraries(pe):
+	########TODO########
+	print(pe.dump_info())
 
 def main(argv):
 	found_i = False
 	found_args = False
 	try:
-		opts, args = getopt.getopt(argv,"hi:o:as:")
+		opts, args = getopt.getopt(argv,"hi:o:as:lx")
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
@@ -38,17 +77,27 @@ def main(argv):
 		if opt == '-h':
 			found_args = True
 			usage()
+			help()
 			sys.exit()
 
       #-i for the input file
 		elif opt in ("-i"):
 			found_i = True
 			pe = pefile.PE(arg)
+			path= arg
 
 		elif opt in ("-a"):
 			found_args=True
 			print(pe.FILE_HEADER)
 			print(pe.OPTIONAL_HEADER)
+
+		elif opt in ("-l"):
+			found_args=True
+			flibraries(pe)
+
+		elif opt in ("-x"):
+			found_args=True
+			fstrings(pe)
 
 		elif opt in ("-s"):
 			found_args = True
